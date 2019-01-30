df34413dfc95188a90272d3be7a24da908038acd
Uladzislau Kaminski <kaminski.uladzislau@gmail.com>
2019-01-30 15:54:21 +0300

    test controller

--- src/main/java/com/uladkaminski/messageservice/controller/TestController.java
+++ src/main/java/com/uladkaminski/messageservice/controller/TestController.java
@@ -7,10 +7,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
-    @Value("${values.test}")
+    @Value("${server.port}")
     private String value;
+
     @GetMapping("/test")
     public String test(){
         return value;
     }
+
 }
